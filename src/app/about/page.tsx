69d2347495296fbbd20a4947d40d28e841fae13f
James Merriman <james.merriman@travelchapter.com>
2026-02-11 14:54:51 +0000

    [amend] improve padding around about image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,7 +20,7 @@ const Page = async () => {
   return (
       <>
         <FullWidthHeader
-          title="About"
+          title="Welcome to my travel writing blog"
           description=""
           breadcrumb={[
             { label: "Home", href: "/" },
@@ -30,7 +30,6 @@ const Page = async () => {
         <main className="container mx-auto mt-8 px-4 max-w-6xl" role="main">
           <div className="flex flex-col-reverse lg:flex-row">
             <div className="w-full lg:w-3/4 prose prose-lg max-w-none break-words blog-content">
-              <h2>Welcome to my travel writing blog</h2>
               <p>
                 I am a Fellow of the Royal Geographical Society and traveller to over 150 countries, who lives in Devon.
               </p>
@@ -38,7 +37,7 @@ const Page = async () => {
                 I love exploring and writing about the world's landscapes, cuisines and cultures.
               </p>
             </div>
-            <div className="w-full lg:w-1/4 pl-4">
+            <div className="w-full lg:w-1/4 pb-4 lg:pb-0 lg:pl-4">
               <Image
                 src="https://avatars.githubusercontent.com/u/20318951?v=4&size=435"
                 width={435}
