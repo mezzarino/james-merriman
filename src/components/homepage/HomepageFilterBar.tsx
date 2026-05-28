540e14513791a6af2d2f0b10379e4dfc39c18a85
James Merriman <james.merriman@travelchapter.com>
2026-05-28 17:44:42 +0100

    [amend] improve filterbar skeleton

--- src/components/homepage/HomepageFilterBar.tsx
+++ src/components/homepage/HomepageFilterBar.tsx
@@ -7,10 +7,27 @@ const FilterBar = dynamic(() => import("../FilterBar").then((m) => m.FilterBar),
 
 function FilterBarSkeleton() {
   return (
-    <div
-      aria-hidden="true"
-      className="my-6 h-11 w-full rounded-md bg-neutral-200 dark:bg-neutral-800"
-    />
+    <div className="w-full px-4 lg:px-0 my-6">
+      <div
+        aria-hidden="true"
+        className="
+          flex flex-col gap-3
+          sm:flex-row sm:items-center sm:justify-between
+        "
+      >
+        {/* Tabs placeholder */}
+        <div className="flex gap-2 overflow-hidden py-1 pr-6 sm:pr-0">
+          {Array.from({ length: 6 }).map((_, i) => (
+            <div key={i} className="h-11 w-24 rounded bg-neutral-200 dark:bg-neutral-800" />
+          ))}
+        </div>
+
+        {/* Search button placeholder */}
+        <div className="flex justify-end sm:justify-start">
+          <div className="h-10 w-10 rounded-md bg-neutral-200 dark:bg-neutral-800" />
+        </div>
+      </div>
+    </div>
   );
 }
 
